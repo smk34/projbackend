d46d3851daefc34a434cb4663e8b184f231bf512
smk34 <20475126+smk34@users.noreply.github.com>
2020-09-04 11:09:45 +0530

    2nd Commit

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-//My Routes 
+//My Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 //DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DB CONNECTED");
@@ -34,6 +35,7 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
--- routes/product.js
+++ routes/product.js
@@ -8,6 +8,8 @@ const {
   photo,
   deleteProduct,
   updateProduct,
+  getAllProducts,
+  getAllUniqueCategories,
 } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
@@ -51,5 +53,7 @@ router.put(
 );
 
 //listing route
+router.get("/products", getAllProducts);
 
+router.get("/products/categories", getAllUniqueCategories);
 module.exports = router;
